9248b483f2e75a3fc3d3dcef422408e06dfe468c
Roland Kozma <roland.kozma@cloudtroopers.com>
2014-12-26 16:10:01 +0200

    parse just the latest ads

--- src/main/java/adsParser/olx/OlxParser.java
+++ src/main/java/adsParser/olx/OlxParser.java
@@ -25,6 +25,8 @@ import adsParser.olx.OlxUrlBuilder.Order;
 
 public class OlxParser implements Parser {
 
+    private static final int PAUSE_TIME = 5000;
+
     private static final Logger LOG = LoggerFactory.getLogger(OlxParser.class);
 
     private static final String REFERENCE_NUMBER_SELECTOR = "div.offerheadinner p small span span span.rel";
@@ -38,7 +40,8 @@ public class OlxParser implements Parser {
     private static final String COMPARTIMENTALIZATION_SELECTOR = "div.descriptioncontent table.details tr:nth-child(1) td:nth-child(2) a";
     private static final String PUBLISHING_DATE_SELECTOR = "div.offerheadinner p small span";
 
-    private static final DateFormat DATE_FORMAT = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.SHORT, new Locale("RO"));
+    private static final Locale LOCALE = new Locale("RO");
+    private static final DateFormat DATE_FORMAT = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.SHORT, LOCALE);
     private static final Pattern DATE_PATTERN = Pattern.compile("\\d{1,2} \\w* \\d{4}");
     private static final Pattern TIME_PATTERN = Pattern.compile("\\d{1,2}:\\d{1,2}");
 
@@ -50,60 +53,70 @@ public class OlxParser implements Parser {
 
     public void parse() {
 	Date currentDate = new Date();
+	Date lastRunDate = getLastRunDate(currentDate);
+	Date publishingDate = currentDate;
 
-	String url = olxUrlBuilder.page(1).getUrl();
-	Document doc = getDocument(url);
+	boolean parseNextPage = true;
+	int pageNumber = 0;
 
-	Elements links = doc.select("a[href]").select(".marginright5").select(".link.linkWithHash").select(".detailsLink");
+	LOG.info("-------------------------------start parsing----------------------------------------------------------\n");
 
-	LOG.debug("got: {}", links.size());
+	while (parseNextPage) {
+	    pageNumber++;
+	    String url = olxUrlBuilder.page(pageNumber).getUrl();
+	    Document doc = getDocument(url);
 
-	for (Element link : links) {
-	    String title = link.text();
-	    LOG.info(title);
+	    Elements links = doc.select("a[href]").select(".marginright5").select(".link.linkWithHash").select(".detailsLink");
+	    LOG.debug("got: {}", links.size());
 
-	    String absUrl = link.absUrl("href");
-	    LOG.info(absUrl);
+	    for (Element link : links) {
+		String title = link.text();
+		LOG.info(title);
 
-	    Document adsDetail = getDocument(absUrl);
+		String absUrl = link.absUrl("href");
+		LOG.info(absUrl);
 
-	    Date publishingDate = getPublishingDate(adsDetail, PUBLISHING_DATE_SELECTOR, "publishingDate");
-	    String providedBy = ParserUtil.getString(adsDetail, PROVIDED_BY_SELECTOR, "providedBy");
-	    String compartimentalization = ParserUtil.getString(adsDetail, COMPARTIMENTALIZATION_SELECTOR, "compartimentalization");
-	    Integer surface = ParserUtil.getInteger(adsDetail, SURFACE_SELECTOR, "surface");
-	    String constructionPeriod = ParserUtil.getString(adsDetail, CONSTRUCTION_PERIOD_SELECTOR, "constructionPeriod");
-	    String description = ParserUtil.getString(adsDetail, DESCRIPTION_SELECTOR, "description");
-	    Integer price = ParserUtil.getInteger(adsDetail, PRICE_SELECTOR, "price");
-	    String phoneNumber = ParserUtil.getString(adsDetail, PHONE_NUMBER_SELECTOR, "phoneNumber");
-	    String userName = ParserUtil.getString(adsDetail, USER_NAME_SELECTOR, "userName");
-	    Integer referenceNumber = ParserUtil.getInteger(adsDetail, REFERENCE_NUMBER_SELECTOR, "referenceNumber");
+		Document adsDetail = getDocument(absUrl);
 
-	    if (isPublishedToday(currentDate, publishingDate)) {
-		LOG.debug("E de azi.");
-	    } else {
-		break;
+		publishingDate = getPublishingDate(adsDetail, PUBLISHING_DATE_SELECTOR, "publishingDate");
+
+		if (publishingDate.before(lastRunDate)) {
+		    parseNextPage = false;
+		    break;
+		}
+
+		String providedBy = ParserUtil.getString(adsDetail, PROVIDED_BY_SELECTOR, "providedBy");
+		String compartimentalization = ParserUtil.getString(adsDetail, COMPARTIMENTALIZATION_SELECTOR, "compartimentalization");
+		Integer surface = ParserUtil.getInteger(adsDetail, SURFACE_SELECTOR, "surface");
+		String constructionPeriod = ParserUtil.getString(adsDetail, CONSTRUCTION_PERIOD_SELECTOR, "constructionPeriod");
+		String description = ParserUtil.getString(adsDetail, DESCRIPTION_SELECTOR, "description");
+		Integer price = ParserUtil.getInteger(adsDetail, PRICE_SELECTOR, "price");
+		String phoneNumber = ParserUtil.getString(adsDetail, PHONE_NUMBER_SELECTOR, "phoneNumber");
+		String userName = ParserUtil.getString(adsDetail, USER_NAME_SELECTOR, "userName");
+		Integer referenceNumber = ParserUtil.getInteger(adsDetail, REFERENCE_NUMBER_SELECTOR, "referenceNumber");
+
+		// TODO persist
+
+		LOG.info("---------------------------------------------------------------------------------------------------\n");
+		pause(PAUSE_TIME);
 	    }
-
-	    pause(2000);
 	}
+
     }
 
-    private boolean isPublishedToday(Date currentDate, Date publishingDate) {
-	boolean isPublishedToday = false;
+    private Date getLastRunDate(Date currentDate) {
+	Date lastRunDate = null;
+	// TODO get last run date from db
+	lastRunDate = getYesterdaysDate(currentDate);
+	LOG.debug("lastRunDate: {}", lastRunDate);
+	return lastRunDate;
+    }
 
-	Calendar currentDateCalendar = Calendar.getInstance();
-	currentDateCalendar.setTime(currentDate);
-
-	Calendar publishingDateCalendar = Calendar.getInstance();
-	publishingDateCalendar.setTime(publishingDate);
-
-	if ((currentDateCalendar.get(Calendar.YEAR) == publishingDateCalendar.get(Calendar.YEAR))
-		&& ((currentDateCalendar.get(Calendar.MONTH) == publishingDateCalendar.get(Calendar.MONTH)))
-		&& ((currentDateCalendar.get(Calendar.DAY_OF_MONTH) == publishingDateCalendar.get(Calendar.DAY_OF_MONTH)))) {
-	    isPublishedToday = true;
-	}
-
-	return isPublishedToday;
+    private Date getYesterdaysDate(Date currentDate) {
+	Calendar calendar = Calendar.getInstance(LOCALE);
+	calendar.setTime(currentDate);
+	calendar.add(Calendar.DATE, -1);
+	return calendar.getTime();
     }
 
     private void pause(long millis) {
